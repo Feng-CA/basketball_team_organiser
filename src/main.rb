5a17eaaa28daab385d97f0334e2ee870e00bc4be
Feng-CA <gcam022106@coderacademy.edu.au>
2021-12-11 15:05:26 +1100

    adds colors to menu options

--- src/main.rb
+++ src/main.rb
@@ -9,15 +9,15 @@ while true
     input = gets.chomp.to_i
     case input 
     when 1 
-        puts "Substituted Player option"
+        puts "Substituted Player option".colorize(:green)
     when 2 
-        puts "Selected Random Teams option"
+        puts "Selected Random Teams option".colorize(:blue)
     when 3 
-        puts "Decided ball possession option"
+        puts "Decided ball possession option".colorize(:yellow)
     when 4
-        puts "Goodbye"
+        puts "Goodbye".colorize(:cyan)
         exit
     else
-        puts "Please pick 1, 2, 3 or 4"
+        puts "Please pick 1, 2, 3 or 4".colorize(:red)
     end
 end
